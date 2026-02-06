b1b3700d9c1482743f074e833ee855993440c06d
Olanrewaju <E.Olanrewaju4233@student.leedsbeckett.ac.uk>
2026-02-06 22:00:50 +0000

    first commit

--- Evaluating_LLM_performance_on_real_dataset.py
+++ Evaluating_LLM_performance_on_real_dataset.py
@@ -1,8 +1,11 @@
 import torch
-from Measuring_LLM_Loss_Function import tokenizer
+import time
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from matplotlib.ticker import MaxNLocator
 from Dataset_and_Dataloader import create_dataloader
-from Coding_the_124M_GPT_2 import model, GPT_CONFIG_124M
-
+from Coding_the_124M_GPT_2 import model, GPT_CONFIG_124M, generate_text_simple, GPTModel
+from Measuring_LLM_Loss_Function import tokenizer, text_to_token_ids, token_ids_to_text
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     text_data = f.read()
@@ -49,10 +52,22 @@ val_loader = create_dataloader(
     batch_size=2,
     max_length=GPT_CONFIG_124M["context_length"],
     stride=GPT_CONFIG_124M["context_length"],
-    drop_last=True,
-    shuffle=True,
+    drop_last=False,
+    shuffle=False,
     num_workers=0,
 )
+
+# Sanity check
+
+if total_tokens * train_ratio < GPT_CONFIG_124M["context_length"]:
+    print("Not enough tokens for the training loader."
+          "Try to lower the GPT_CONFIG_124M[ context length] or."
+          "increase the training_ratio")
+
+if total_tokens * (1-train_ratio) < GPT_CONFIG_124M["context_length"]:
+    print("Not enough tokens for the validation loader."
+          "Try to lower the GPT_CONFIG_124M[ context length] or."
+          "increase the training_ratio")
 
 print("Train loader: ")
 for x, y in train_loader:
@@ -88,8 +103,8 @@ def calc_loss_batch(input_batch, target_batch, model, device):
     return loss
 
 #Loss for multiple batches
-def calc_loss_loader(data_loader, model, device, num_batches):
-    total_loss = 0
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
     if len(data_loader) == 0:
         return float("nan")
     elif num_batches is None:
@@ -127,8 +142,104 @@ model.to(device) # no assignment model = model.to(device) necessary for nn.Modul
 torch.manual_seed(123) # For reproducibility due to the shuffling in the data loader
 
 with torch.no_grad(): # Disable gradient tracking for efficiency because we are not training
-    train_loss = calc_loss_loader(train_loader, model, device, num_batches=None)
-    val_loss = calc_loss_loader(val_loader, model, device, num_batches=None)
+    train_loss = calc_loss_loader(train_loader, model, device)
+    val_loss = calc_loss_loader(val_loader, model, device)
 
 print("Training loss: ", train_loss)
 print("Validation loss: ", val_loss)
+
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
+    # Initialize lists to track losses and tokens seen
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    #main training loop
+    for epoch in range(num_epochs):
+        model.train()  #set model to training mode
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad() # reset loss gradients from previous batch iteration
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward() # Calculate loss gradients
+            optimizer.step() # Update model weights using loss gradients
+            tokens_seen += input_batch.numel() # Returns the total number of elements (or tokens) in the input_batch
+            global_step += 1
+
+            # Optional evaluation step
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep{epoch+1} (Step{global_step:06d}): "
+                      f"Train Loss: {train_loss:.3f}, Val Loss: {val_loss:.3f} ")
+
+        # Print a sample text after each epoch
+        generate_and_print_sample(
+            model,tokenizer,device, start_context
+        )
+
+# This evaluate_model code calculates the loss over the training and validation set while ensuring that the model is in eval mode
+# so that dropout is disabled when calculating the loss over the training and validation sets
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+
+# This function lets us generate the text after every epoch
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded,
+            max_new_tokens=50, context_size=context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n ", "")) # Compact print format
+    model.train()
+
+
+# Let's see all this in action by training a GPTModel instance for 10 epochs, using AdamW optimizer
+
+
+start_time = time.time()
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+
+num_epochs = 10
+
+train_model_simple(
+    model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+    start_context="Every effort moves you", tokenizer=tokenizer
+)
+
+# Note:
+# Uncomment the following code to show the execution time
+end_time = time.time()
+execution_time_minutes = (end_time - start_time) / 60
+print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # Plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_losses, label="Training Loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation Loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True)) # only
+
+    epochs_tensor = torch.tensor(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
